39299b2fa0afe3d97cc81757a92fa73171cee004
penny-wanjiru <wanjirupenina@gmail.com>
2024-06-12 13:37:03 +0300

    section off reading list and make it scrollable

--- src/frontend/components/ReadingList.js
+++ src/frontend/components/ReadingList.js
@@ -7,18 +7,20 @@ const ReadingList = ({ books, onRemove }) => {
   return (
     <div className="reading-list">
       <h2>READING LIST</h2>
-      <List>
-        {books.map((book) => (
-          <ListItem key={book.title}>
-            <ListItemText primary={book.title} secondary={book.author} />
-            <ListItemSecondaryAction>
-              <Button variant="contained" style={{ backgroundColor: theme.palette.secondary.orangeRed, color: theme.palette.secondary.pastel }} onClick={() => onRemove(book.title)}>
-                Remove
-              </Button>
-            </ListItemSecondaryAction>
-          </ListItem>
-        ))}
-      </List>
+      <div className='reading-list-container'>
+        <List>
+          {books.map((book) => (
+            <ListItem key={book.title}>
+              <ListItemText primary={book.title} secondary={book.author} />
+              <ListItemSecondaryAction>
+                <Button variant="contained" style={{ backgroundColor: theme.palette.secondary.orangeRed, color: theme.palette.secondary.pastel }} onClick={() => onRemove(book.title)}>
+                  Remove
+                </Button>
+              </ListItemSecondaryAction>
+            </ListItem>
+          ))}
+        </List>
+      </div>
     </div>
   );
 };
